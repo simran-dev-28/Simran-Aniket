051779c8ad912fd937f9ea2268155f18c4e50669
Simran Bhamrah <36455112+simran-dev-28@users.noreply.github.com>
2026-06-26 19:58:34 +0530

    Delete js directory

--- js/counter.js
+++
@@ -1,5 +0,0 @@
-
-const start=new Date('2023-10-17');
-const days=Math.floor((new Date()-start)/(1000*60*60*24));
-document.getElementById('counter').innerHTML=days+' Days Together ❤️';
-const d=document.getElementById('days'); if(d)d.innerHTML=days;
